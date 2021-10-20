6f99910eb23bf63403effcdeaa093ead62011da3
eeeibra <eeeibrahimsahin@gmail.com>
2021-10-20 10:45:10 +0200

    Added rate functionality.

--- src/app/components/rate-movie/rate-movie.component.ts
+++ src/app/components/rate-movie/rate-movie.component.ts
@@ -1,13 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-rate-movie',
   templateUrl: './rate-movie.component.html',
   styleUrls: ['./rate-movie.component.css']
 })
-export class RateMovieComponent implements OnInit {
+export class RateMovieComponent implements OnInit,OnChanges {
 
-  constructor() { }
+  @Input() rating: number = 0;
+  cropWidth: number = 75;
+  @Output() ratingClicked: EventEmitter<string> =
+    new EventEmitter<string>();
+
+  ngOnChanges(): void {
+    this.cropWidth = this.rating * 75 / 5;
+  }
+
+  onClick(): void {
+    this.ratingClicked.emit(`The rating ${this.rating} was clicked!`);
+  }
 
   ngOnInit(): void {
   }
